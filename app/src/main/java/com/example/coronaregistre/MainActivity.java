b5cdf2ab630b5bbf7260722bff2abcbe839aaf23
zinamtaallah <zinamtaallah1@gmail.com>
2020-11-22 20:52:41 +0100

    CoronaRegistre

--- app/src/main/java/com/example/coronaregistre/MainActivity.java
+++ app/src/main/java/com/example/coronaregistre/MainActivity.java
@@ -6,15 +6,11 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.widget.Toolbar;
 
+
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar =findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-    }
+    
+
 
     private void setSupportActionBar(Toolbar toolbar) {
     }
