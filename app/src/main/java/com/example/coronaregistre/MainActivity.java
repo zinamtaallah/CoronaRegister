c0fc79fb2bd2eee0526bb903b20829caa0f6c25d
zinamtaallah <zinamtaallah1@gmail.com>
2020-12-31 11:15:29 +0100

    CoronaRegistre

--- app/src/main/java/com/example/coronaregistre/MainActivity.java
+++ app/src/main/java/com/example/coronaregistre/MainActivity.java
@@ -2,40 +2,33 @@ package com.example.coronaregistre;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.coronaregistre.R;
 
 
 public class MainActivity extends AppCompatActivity {
 
 
     public ImageView play;
-    protected  void onCreate(Bundle savedInstanceState){
+    protected  void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-          View changelang = findViewById(R.id.changeMylang);
-          changelang.setOnClickListener(new View.OnClickListener(){
+        View changelang = findViewById(R.id.changeMylang);
+        changelang.setOnClickListener(v -> showChangelanguedialog());
 
 
-              @Override
-              public void onClick(View v) {
-                  showChangelanguedialog();
-                  
-              }
-          });
-
-        this.play=  (ImageView) findViewById(R.id.play);
-        play.setOnClickListener(new View.OnClickListener(){
+        this.play = (ImageView) findViewById(R.id.play);
+        play.setOnClickListener(new View.OnClickListener() {
 
 
             @Override
             public void onClick(View v) {
-                Intent otherActivity=new Intent(getApplicationContext(),protection.class) ;
+                Intent otherActivity = new Intent(getApplicationContext(), protection.class);
                 startActivity(otherActivity);
 
                 finish();
@@ -43,9 +36,8 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-
-
     }
+
 
     private void showChangelanguedialog() {
     }
--- app/src/main/java/com/example/coronaregistre/protection.java
+++ app/src/main/java/com/example/coronaregistre/protection.java
@@ -1,4 +1,5 @@
 package com.example.coronaregistre;
 
 public class protection {
+
 }
